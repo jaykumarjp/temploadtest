f18e73df6651109c38f9bfc95f2d3a3631890a9a
jaykumarjp <42172283+jaykumarjp@users.noreply.github.com>
2022-12-17 20:14:23 +0530

    Update load.py

--- load.py
+++ load.py
@@ -5,9 +5,13 @@ class WebsiteUser(HttpUser):
     wait_time = between(5, 15)
 
     def on_start(self):
-        self.client.post("/user/auth/v2/send-otp", {
+        response=self.client.post("/user/auth/v2/send-otp", {
             "mobile": "9999642347"
         })
+        json_var = response.json()
+        request_id = json_var['title']
+
+        print('Post title is ' + request_id)
 
     @task
     def index(self):
