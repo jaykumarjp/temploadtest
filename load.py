2c7a81e3b7af7d11b2e7f0a727c8473c916a5852
jaykumarjp <42172283+jaykumarjp@users.noreply.github.com>
2022-12-17 20:38:56 +0530

    Update load.py

--- load.py
+++ load.py
@@ -2,7 +2,7 @@ from locust import HttpUser, between, task
 
 
 class WebsiteUser(HttpUser):
-    wait_time = between(5, 15)
+    wait_time = between(0.05, 0.1)
 
     def on_start(self):
         response=self.client.post("/user/auth/v2/send-otp", {
