b8b1df5045d17f607bc66c8882fbbcc17736b802
jaykumarjp <42172283+jaykumarjp@users.noreply.github.com>
2022-12-18 10:32:05 +0530

    Update load.py

--- load.py
+++ load.py
@@ -3,16 +3,6 @@ from locust import HttpUser, between, task
 class WebsiteUser(HttpUser):
     wait_time = between(0.05, 0.1)
 
-    def on_start(self):
-        response=self.client.post("/user/auth/v2/send-otp", {
-            "mobile": "7014090407",
-            "ccode": "+91"
-        })
-        json_var = response.json()
-
-        print(str(json_var))
-        
-
     @task
     def index(self):
         response=self.client.post("/user/auth/v2/send-otp", {
@@ -20,7 +10,7 @@ class WebsiteUser(HttpUser):
             "ccode": "+91"
         })
         
-        json_var = response.json()
+        #json_var = response.json()
 
-        print(str(json_var))
+        print(str(response))
         
